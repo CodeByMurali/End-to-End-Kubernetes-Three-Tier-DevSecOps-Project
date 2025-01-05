3769788ad42459ddb053c1fe269053c0bd6dc1f3
murali.rajendran <murali.rajendran@veeva.com>
2025-01-04 23:00:24 -0500

    Modify bucket name

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -3,13 +3,13 @@ terraform {
   backend "s3" {
     bucket         = "use1-remote-terraform-state-file-bucket"
     region         = "us-east-1"
-    key            = "Project-2-Three-Tier-DevSecOps-Pipeline-Lock-Files/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Project-2-Three-Tier-DevSecOps-Pipeline-Lock-Files-Jenkins-Server"
+    key            = "Jenkins-Server-TF/terraform.tfstate"
+    dynamodb_table = "Project-2-Three-Tier-DevSecOps-Pipeline-Lock-Files-EKS"
     encrypt        = true
   }
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.82.2"
     }
   }
